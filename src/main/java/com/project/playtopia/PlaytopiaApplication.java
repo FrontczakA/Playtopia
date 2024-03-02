2f411585a7a54dd3bcebb69d4185937661e9a6c5
altut <frontczak-artur@wp.pl>
2024-03-02 11:48:35 +0100

    new project creation

---
+++ src/main/java/com/project/playtopia/PlaytopiaApplication.java
@@ -0,0 +1,13 @@
+package com.project.playtopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaytopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaytopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/playtopia/PlaytopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.playtopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaytopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
