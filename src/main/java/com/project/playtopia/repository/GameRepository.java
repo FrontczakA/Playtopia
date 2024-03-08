fc50c561d67d3df61ed2ff7ffd371d2d70e5f0c4
altut <frontczak-artur@wp.pl>
2024-03-08 19:20:32 +0100

    Added 'All' console option to filtering

--- src/main/java/com/project/playtopia/repository/GameRepository.java
+++ src/main/java/com/project/playtopia/repository/GameRepository.java
@@ -10,6 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    @Query("SELECT g FROM Game g WHERE (:min IS NULL OR g.price > :min) AND (:max IS NULL OR g.price < :max) AND (:console IS NULL OR g.platform = :console) AND (:category='' OR g.genre = :category)")
+    @Query("SELECT g FROM Game g WHERE (:min IS NULL OR g.price > :min) AND (:max IS NULL OR g.price < :max) AND (:console=''  OR g.platform = :console) AND (:category='' OR g.genre = :category)")
     Page<Game> filterGamesList(double min, double max, String console, String category, Pageable pageable);
 }
