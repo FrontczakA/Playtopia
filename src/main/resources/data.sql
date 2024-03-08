87f89f88715a67831580b335357c01a824e6d6d1
altut <frontczak-artur@wp.pl>
2024-03-08 18:53:25 +0100

    Added photos to test data, list display correction in css file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,25 +14,23 @@ FROM (
          UNION ALL
          SELECT 39.99, 'Total War: Napoleon', 'Strategic turn-based game from the Total War series. Lead Napoleon Bonaparte and conquer Europe in epic battles.', 'Strategy', 'PC', 'https://wiki.totalwar.com/images/thumb/b/b0/Ntw_ge.jpg/500px-Ntw_ge.jpg', 60
          UNION ALL
-         SELECT 49.99, 'Assassins Creed Valhalla', 'Embark on a Viking journey in the latest Assassins Creed installment. Raid villages, build your settlement, and lead your clan to glory.', 'Action-Adventure', 'XBOX SERIES X', 'https://placehold.co/600x400', 70
+         SELECT 49.99, 'Assassins Creed Valhalla', 'Embark on a Viking journey in the latest Assassins Creed installment. Raid villages, build your settlement, and lead your clan to glory.', 'Action-Adventure', 'XBOX SERIES X', 'https://m.media-amazon.com/images/I/7131E1EEERL._AC_UF1000,1000_QL80_.jpg', 70
          UNION ALL
-         SELECT 59.99, 'Cyberpunk 2077', 'Immerse yourself in a futuristic open-world RPG. Explore the bustling streets of Night City and uncover its dark secrets.', 'RPG', 'PC', 'https://placehold.co/600x400', 40
+         SELECT 59.99, 'Cyberpunk 2077', 'Immerse yourself in a futuristic open-world RPG. Explore the bustling streets of Night City and uncover its dark secrets.', 'RPG', 'PC', 'https://m.media-amazon.com/images/I/81qAWv0I4CL._AC_UF894,1000_QL80_.jpg', 40
          UNION ALL
-         SELECT 39.99, 'Minecraft', 'Create and explore your own blocky world in this sandbox game. Build structures, mine resources, and unleash your creativity!', 'Adventure', 'Switch', 'https://placehold.co/600x400', 90
+         SELECT 39.99, 'Minecraft', 'Create and explore your own blocky world in this sandbox game. Build structures, mine resources, and unleash your creativity!', 'Adventure', 'Switch', 'https://m.media-amazon.com/images/I/81gJUSx-TjL.jpg', 90
          UNION ALL
-         SELECT 29.99, 'Rocket League', 'Unique combination of soccer and vehicular mayhem. Score goals, perform aerial acrobatics, and customize your car in this fast-paced game!', 'Sports', 'PS4', 'https://placehold.co/600x400', 110
+         SELECT 29.99, 'Rocket League', 'Unique combination of soccer and vehicular mayhem. Score goals, perform aerial acrobatics, and customize your car in this fast-paced game!', 'Sports', 'PS4', 'https://a.allegroimg.com/original/1160d6/64fb2f8e4146b9d6faf1f81af187/SONY-PS4-ROCKET-LEAGUE', 110
          UNION ALL
-         SELECT 59.99, 'Call of Duty: Warzone', 'Free-to-play battle royale experience set in the Call of Duty universe. Drop into massive maps, gather weapons, and outlast your opponents!', 'Shooter', 'PC', 'https://placehold.co/600x400', 60
+         SELECT 59.99, 'Call of Duty: Modern Warfare 3', 'Free-to-play battle royale experience set in the Call of Duty universe. Drop into massive maps, gather weapons, and outlast your opponents!', 'Shooter', 'PC', 'https://5.allegroimg.com/s1024/0c5cb6/7d22876943f7a292b56b534b3c35', 60
          UNION ALL
-         SELECT 49.99, 'Animal Crossing: New Horizons', 'Escape to a deserted island and create your own paradise in this charming life simulation game. Decorate your home, make friends, and enjoy island life!', 'Simulation', 'Switch', 'https://placehold.co/600x400', 70
+         SELECT 49.99, 'Animal Crossing: New Horizons', 'Escape to a deserted island and create your own paradise in this charming life simulation game. Decorate your home, make friends, and enjoy island life!', 'Simulation', 'Switch', 'https://www.mediaexpert.pl/media/cache/resolve/gallery/images/21/2161785/Animal-Crossing-New-Horizons-Gra-NINTENDO-SWITCH-front.jpg', 70
          UNION ALL
-         SELECT 39.99, 'God of War', 'Embark on a journey with Kratos, a fallen god seeking redemption. Battle mythical creatures, solve puzzles, and explore a stunning world inspired by Norse mythology.', 'Action-Adventure', 'PS4', 'https://placehold.co/600x400', 80
+         SELECT 39.99, 'God of War', 'Embark on a journey with Kratos, a fallen god seeking redemption. Battle mythical creatures, solve puzzles, and explore a stunning world inspired by Norse mythology.', 'Action-Adventure', 'PS4', 'https://6.allegroimg.com/s1024/0c623b/8047721d4fa6a44bff27c8e4d646', 80
          UNION ALL
-         SELECT 49.99, 'League of Legends', 'Popular multiplayer online battle arena game. Choose from a variety of champions, strategize with your team, and destroy the enemy nexus!', 'MOBA', 'PC', 'https://placehold.co/600x400', 100
+         SELECT 29.99, 'Among Us', 'Social deduction game where players work together to complete tasks on a spaceship, while trying to identify impostors among them. Can you survive?', 'Party', 'PC', 'https://cdn1.epicgames.com/salesEvent/salesEvent/amogusportrait_1200x1600-66ad0e4d363e1c92f9f8aae67a96dd31', 120
          UNION ALL
-         SELECT 29.99, 'Among Us', 'Social deduction game where players work together to complete tasks on a spaceship, while trying to identify impostors among them. Can you survive?', 'Party', 'PC', 'https://placehold.co/600x400', 120
-         UNION ALL
-         SELECT 39.99, 'The Legend of Zelda: Breath of the Wild', 'Explore the vast kingdom of Hyrule in this epic adventure game. Solve puzzles, defeat enemies, and uncover the secrets of the ancient land.', 'Action-Adventure', 'Switch', 'https://placehold.co/600x400', 70
+         SELECT 39.99, 'The Legend of Zelda: Breath of the Wild', 'Explore the vast kingdom of Hyrule in this epic adventure game. Solve puzzles, defeat enemies, and uncover the secrets of the ancient land.', 'Action-Adventure', 'Switch', 'https://image.ceneostatic.pl/data/products/49538999/i-the-legend-of-zelda-breath-of-the-wild-gra-ns.jpg', 70
      ) AS data
          LEFT JOIN game ON game.title = data.title
 WHERE game.title IS NULL;
