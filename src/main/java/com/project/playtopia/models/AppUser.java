22573d04721bba9a372e65542c26fb54317935ad
altut <frontczak-artur@wp.pl>
2024-03-11 17:44:39 +0100

    Enhanced login and registration form validation, and integrated Spring Security Thymeleaf add-on to conceal unauthenticated content from the layout.

--- src/main/java/com/project/playtopia/models/AppUser.java
+++ src/main/java/com/project/playtopia/models/AppUser.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,15 +17,23 @@ public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     @NotEmpty(message ="This field can't be empty!")
     String username;
+
     @NotEmpty(message ="This field can't be empty!")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     String password;
+
     @NotEmpty(message ="This field can't be empty!")
+    @Email(message = "Please provide a valid email address")
     String email;
+
     @NotEmpty(message ="This field can't be empty!")
     String name;
+
     @NotEmpty(message ="This field can't be empty!")
     String surname;
+
     String role;
 }
